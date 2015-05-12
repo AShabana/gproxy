f87b9fcb2042ddc64f4876f8f59df4891e07393f
AShabana <AhmedShabana2000@gmail.com>
2015-05-12 10:19:06 +0300

    Update create_fake_firsttime_startup.py

--- testing_tools/create_fake_firsttime_startup.py
+++ testing_tools/create_fake_firsttime_startup.py
@@ -1,21 +1,21 @@
-	  import os
-	  
-	  print "This script MUST run from "
-	  print os.path.dirname(__file__)
-	
-		accounts = ( 'ahmed' ,'ali' ,'omar')
-		with open('../config/lists/accounts','wb') as fd :
-		       pickle.dump(accounts, fd)
-		       
-		config_data = """ 
-		logic = "KannelProxy"
-		lists = "config/lists/accounts"
-		counter-path = "counters"
-		ports = "2014,2015,2016,2017,2018"
-		log-file = "/var/log/GProxy.log"
-		pid-file = "listener.pid"
-		"""
-		
-		with open('../config/app.conf', 'w') as config_file :
-		    config_file.write(config_data)
+import os
+import pickle
+print "This script MUST run from script directory "
+
+
+accounts = ( 'ahmed' ,'ali' ,'omar')
+with open('../config/lists/accounts','wb') as fd :
+       pickle.dump(accounts, fd)
+       
+config_data = """ 
+logic = "KannelProxy"
+lists = "config/lists/accounts"
+counter-path = "counters"
+ports = "2014,2015,2016,2017,2018"
+log-file = "/var/log/GProxy.log"
+pid-file = "listener.pid"
+"""
+
+with open('../config/app.conf', 'w') as config_file :
+    config_file.write(config_data)
 		 
